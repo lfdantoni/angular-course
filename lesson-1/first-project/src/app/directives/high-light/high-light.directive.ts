76338583e62a6434043a61bc18cfef759deaa491
lfdantoni <lfdantoni@gmail.com>
2019-06-06 22:14:23 -0300

    lesson-1 added highlight events

--- lesson-1/first-project/src/app/directives/high-light/high-light.directive.ts
+++ lesson-1/first-project/src/app/directives/high-light/high-light.directive.ts
@@ -1,12 +1,23 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighLight]'
 })
 export class HighLightDirective {
 
-  constructor(el: ElementRef) { 
-    el.nativeElement.style.backgroundColor = 'yellow';
+  constructor(private el: ElementRef) { 
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+ 
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
   }
 
 }
