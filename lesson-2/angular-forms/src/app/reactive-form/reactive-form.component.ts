f49194634b92eefa5ca300ba44613fbd97114b21
lfdantoni <lfdantoni@gmail.com>
2019-06-11 20:15:34 -0300

    lesson-2 added validation in reactive form

--- lesson-2/angular-forms/src/app/reactive-form/reactive-form.component.ts
+++ lesson-2/angular-forms/src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -13,13 +13,25 @@ export class ReactiveFormComponent implements OnInit {
 
   ngOnInit() {
     this.formGroup = new FormGroup({
-      'firstName': new FormControl(''),
-      'lastName': new FormControl('')
+      'firstName': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'lastName': new FormControl('', Validators.required), // array is not necessary for one validator
+      'email': new FormControl('', [
+        Validators.required,
+        Validators.email
+      ])
     })
   }
 
   onSubmit() {
     console.log(this.formGroup.value);
+    console.log(this.firstName.errors);
+  }
+
+  get firstName(): AbstractControl {
+    return this.formGroup.get('firstName');
   }
 
 }
