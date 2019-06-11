0658453fbdd7aad50eeadc5b8957a2d2529e2573
lfdantoni <lfdantoni@gmail.com>
2019-06-11 19:28:17 -0300

    lesson-2 added teplate-driven form component

--- lesson-2/angular-forms/src/app/app.module.ts
+++ lesson-2/angular-forms/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormComponent
+    ReactiveFormComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule, // reactive forms
+    FormsModule  // Template-Driven forms
   ],
   providers: [],
   bootstrap: [AppComponent]
