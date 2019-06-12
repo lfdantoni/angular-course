629953a225bccd0989d7fd578c492724bbfe5cde
lfdantoni <lfdantoni@gmail.com>
2019-06-12 20:27:55 -0300

    lesson-2 added reactive form custom validators

--- lesson-2/angular-forms/src/app/reactive-form/reactive-form.component.ts
+++ lesson-2/angular-forms/src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { idValidator } from '../validators/id-validator';
 
 @Component({
   selector: 'app-reactive-form',
@@ -21,7 +22,9 @@ export class ReactiveFormComponent implements OnInit {
       'email': new FormControl('', [
         Validators.required,
         Validators.email
-      ])
+      ]),
+      'phoneNumber': new FormControl('', this.telephoneValidator(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/)),
+      'id': new FormControl('', idValidator),
     })
   }
 
@@ -30,8 +33,22 @@ export class ReactiveFormComponent implements OnInit {
     console.log(this.firstName.errors);
   }
 
+  telephoneValidator(phoneExp: RegExp): ValidatorFn {
+    return (control: FormControl): {[key: string]: any} => {
+      const isValid = phoneExp ? phoneExp.test(control.value) : true;
+      return !isValid ? {'phoneNumber':  {value: control.value} } : null;
+    }
+  }
+
   get firstName(): AbstractControl {
     return this.formGroup.get('firstName');
   }
 
+  get phoneNumber(): AbstractControl {
+    return this.formGroup.get('phoneNumber');
+  }
+
+  get id(): AbstractControl {
+    return this.formGroup.get('id');
+  }
 }
