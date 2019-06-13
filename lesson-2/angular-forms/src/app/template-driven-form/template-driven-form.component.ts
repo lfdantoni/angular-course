441bdea937a6ebbbfdafb2313bec4697b76ae69b
lfdantoni <lfdantoni@gmail.com>
2019-06-12 21:01:10 -0300

    lesson-2 added 2 way data binding example

--- lesson-2/angular-forms/src/app/template-driven-form/template-driven-form.component.ts
+++ lesson-2/angular-forms/src/app/template-driven-form/template-driven-form.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+class MyModel {
+  comment?: string;
+}
+
 @Component({
   selector: 'app-template-driven-form',
   templateUrl: './template-driven-form.component.html',
   styleUrls: ['./template-driven-form.component.css']
 })
 export class TemplateDrivenFormComponent implements OnInit {
+  myModel: MyModel = {};
 
   constructor() { }
 
@@ -14,5 +19,6 @@ export class TemplateDrivenFormComponent implements OnInit {
 
   onSubmit(value: any) {
     console.log(value);
+    console.log(this.myModel);
   }
 }
