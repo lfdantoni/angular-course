a3c45fc7b8539360f472ee2af71be2981da5f6d6
lfdantoni <lfdantoni@gmail.com>
2019-06-18 20:02:57 -0300

    lesson-3 added useValue examples

--- lesson-3/viutube-project/src/app/app.module.ts
+++ lesson-3/viutube-project/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListVideosComponent } from './components/list-videos/list-videos.compon
 import { HighLightDirective } from './directives/high-light/high-light.directive';
 import { LoggerService } from './services/logger/logger.service';
 import { PageProperties } from './models/PageProperties';
+import { TITLE } from './services/injection-tokens';
 
 const pageProperties: PageProperties = {
   title: 'ViuTube',
@@ -27,7 +28,8 @@ const pageProperties: PageProperties = {
   ],
   providers: [
     LoggerService, // It is the same "provideIn: 'root'"
-    {provide: PageProperties, useValue: pageProperties} // set a page properties object as PageProperties class dependency
+    {provide: PageProperties, useValue: pageProperties}, // set a page properties object as PageProperties class dependency
+    {provide: TITLE, useValue: 'ViuTube'} // set a page title as constant dependency with TITLE as the token injection key
   ],
   bootstrap: [AppComponent]
 })
--- lesson-3/viutube-project/src/app/components/list-videos/list-videos.component.ts
+++ lesson-3/viutube-project/src/app/components/list-videos/list-videos.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import Video from '../../models/video';
 import { VideoService } from 'src/app/services/video/video.service';
 import { LoggerService } from 'src/app/services/logger/logger.service';
 import { PageProperties } from 'src/app/models/PageProperties';
+import { TITLE } from 'src/app/services/injection-tokens';
 
 @Component({
   selector: 'app-list-videos',
@@ -14,7 +15,8 @@ export class ListVideosComponent implements OnInit {
 
   constructor(private videoService: VideoService, 
               private logger: LoggerService,
-              private pageProperties: PageProperties) {
+              private pageProperties: PageProperties,
+              @Inject(TITLE) title: string) {
     // const videoService = new VideoService(); // new service instance, it is NOT the same of the load one
     this.videos = videoService.videos;
     this.logger.log({
@@ -23,6 +25,7 @@ export class ListVideosComponent implements OnInit {
     })
 
     console.log('ListVideosComponent constructor -> pageProperties: ', this.pageProperties);
+    console.log('ListVideosComponent constructor -> TITLE: ', title);
   }
 
   ngOnInit() {
