4e3796c529909b3b9a68bc7b1e00f34ca7697dca
Leonardod <lfdantoni@gmail.com>
2020-07-16 20:45:42 -0300

    updated folders structure

--- lessons/e2e/src/app.e2e-spec.ts
+++ lessons/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('first-project app is running!');
+    expect(page.getTitleText()).toEqual('lessons app is running!');
   });
 
   afterEach(async () => {
--- lessons/src/app/app.component.spec.ts
+++ lessons/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first-project'`, () => {
+  it(`should have as title 'lessons'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-project');
+    expect(app.title).toEqual('lessons');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('first-project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('lessons app is running!');
   });
 });
