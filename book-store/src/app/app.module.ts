aeaece66e2e88361208760054155b87aa7553d8e
Leonardod <lfdantoni@gmail.com>
2020-07-23 18:40:48 -0300

    book-store restructured components

--- book-store/src/app/app.module.ts
+++ book-store/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
-import { FilterComponent } from './filter/filter.component';
+import { FilterComponent } from './book-list/filter/filter.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookComponent } from './book-list/book/book.component';
 import { BookStatusDirective } from './book-list/book/book-status.directive';
