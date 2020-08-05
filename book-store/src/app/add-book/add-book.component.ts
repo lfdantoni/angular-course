db6992eaf0ca268f2740d4dfa40db6e7df391a6f
Leonardod <lfdantoni@gmail.com>
2020-08-04 22:37:31 -0300

    added form missing fields

--- book-store/src/app/add-book/add-book.component.ts
+++ book-store/src/app/add-book/add-book.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { imageUrlValidator } from '../validators/image-url-validator';
+import { BookService } from '../services/book/book.service';
 
+
+export interface CategoryItemModel{
+  value: string;
+  display: string;
+  checked: boolean;
+}
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
@@ -9,28 +16,55 @@ import { imageUrlValidator } from '../validators/image-url-validator';
 })
 export class AddBookComponent implements OnInit {
   formGroup: FormGroup;
+  categoryList: CategoryItemModel[] = [];
 
-  constructor() {
+  constructor(private bookService: BookService) {
     this.formGroup = new FormGroup({
+      id: new FormControl('', Validators.required),
       title: new FormControl('', Validators.required),
       isbn: new FormControl('', [Validators.required, Validators.pattern(/[0-9]*[-| ][0-9]*[-| ][0-9]*[-| ][0-9]*[-| ][0-9]*/)]),
       author: new FormControl('', [Validators.required, this.authorValidator]),
       cover: new FormControl('', [Validators.required, imageUrlValidator]),
-      comment: new FormControl('')
+      stock: new FormControl(null, [Validators.required, Validators.pattern(/[0-9]+/), Validators.min(0)]),
+      price: new FormControl(null, [Validators.required, Validators.pattern(/[0-9.]+/), Validators.min(0)]),
+      categories: new FormControl([])
     });
   }
 
   ngOnInit(): void {
+    const categories = this.bookService.getCategories();
+
+    this.categoryList = categories.map(cat => ({
+      value: cat.code,
+      display: cat.display,
+      checked: false
+    }));
   }
 
-  onSubmit() {
+  onSubmit(): void {
     console.log('Submit ', this.formGroup.value);
     this.formGroup.reset();
+    this.categories.setValue([]);
+    this.categoryList.forEach(cat => cat.checked = false);
   }
 
   authorValidator(control: FormControl): ValidationErrors {
     const valid = control.value ? control.value.toString().length > 5 : false;
     return !valid ? {author: control.value} : null;
+  }
+
+  onCategoryChange(category: CategoryItemModel): void {
+    category.checked = !category.checked;
+
+    if (category.checked) {
+      this.categories.setValue([category.value].concat(this.categories.value));
+    } else {
+      const currentCategories: string[] = this.categories.value;
+
+      currentCategories.splice(currentCategories.findIndex(cat => cat === category.value), 1);
+
+      this.categories.setValue(currentCategories);
+    }
   }
 
   get title(): AbstractControl {
@@ -47,5 +81,21 @@ export class AddBookComponent implements OnInit {
 
   get cover(): AbstractControl {
     return this.formGroup.get('cover');
+  }
+
+  get id(): AbstractControl {
+    return this.formGroup.get('id');
+  }
+
+  get stock(): AbstractControl {
+    return this.formGroup.get('stock');
+  }
+
+  get price(): AbstractControl {
+    return this.formGroup.get('price');
+  }
+
+  get categories(): AbstractControl {
+    return this.formGroup.get('categories');
   }
 }
