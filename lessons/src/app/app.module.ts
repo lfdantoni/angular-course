1fd0e3a251e2e5cd00ae4b4c0f150f87cc43160a
lfdantoni <lfdantoni@gmail.com>
2022-01-02 15:06:47 -0300

    Add data binding component

--- lessons/src/app/app.module.ts
+++ lessons/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { BookComponent } from './data-binding/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
