f6dcf1156ad811504eaeb69a6daa4ef9f8dea768
lfdantoni <lfdantoni@gmail.com>
2022-02-04 18:35:09 -0300

    Add map operator examples

--- lessons/src/app/book-list/book-list.component.ts
+++ lessons/src/app/book-list/book-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { forkJoin, Subscription } from 'rxjs';
-import { first } from 'rxjs/operators';
+import { first, map } from 'rxjs/operators';
 import { Book } from '../models/book';
 import { BookCategory } from '../models/book-category';
 import { BookService } from '../services/book/book.service';
@@ -42,14 +42,26 @@ export class BookListComponent implements OnInit, OnDestroy {
     //     this.isLoading = false;
     //   });
 
+    // forkJoin([ this.bookService.getBooks(),  this.bookService.getCategories()])
+    //   .pipe(first())
+    //   .subscribe(([books, categories]) => {
+    //     this.bookList = books;
+    //     this.bookCategories = categories;
+    //     this.isLoading = false;
+    //   },
+    //   error => console.log('ERROR!', error))
+
     forkJoin([ this.bookService.getBooks(),  this.bookService.getCategories()])
       .pipe(first())
-      .subscribe(([books, categories]) => {
-        this.bookList = books;
-        this.bookCategories = categories;
-        this.isLoading = false;
-      },
-      error => console.log('ERROR!', error))
+      .subscribe({
+        next: ([books, categories]) => {
+          this.bookList = books;
+          this.bookCategories = categories;
+          this.isLoading = false;
+        },
+        error: error => console.log('ERROR!', error),
+        complete: () => console.log('Execution complete!'),
+      })
   }
 
   ngOnDestroy(): void {
@@ -62,18 +74,39 @@ export class BookListComponent implements OnInit, OnDestroy {
 
   onFilterChange(target: EventTarget | null) {
     const value = target ? (target as HTMLSelectElement).value : '';
+    const valueToFilter = value === 'all' ? '' : value;
 
     console.log('onFilterChange', value)
     if(!value) return;
 
     this.isBooksLoading = true;
 
-    this.bookService.getBooksByCategory(value === 'all' ? '' : value)
+    this.bookService.getBooksByCategory(valueToFilter)
       .pipe(first())
       .subscribe(books => {
         this.bookList = books;
         this.isBooksLoading = false;
       })
+
+      // Map example
+      // this.bookService.getBooksByCategory(valueToFilter)
+      // .pipe(first(), map(books => books.map(book => book.title)))
+      // .subscribe(books => {
+      //   console.log(books)
+      // })
+
+      // Map with array filter
+      // this.bookService.getBooks()
+      // .pipe(
+      //   first(),
+      //   map(books=>
+      //     books.filter(book => valueToFilter ?
+      //       book.categories.includes(valueToFilter) : true)))
+      // .subscribe(books => {
+      //   console.log(books)
+      //   this.bookList = books;
+      //   this.isBooksLoading = false;
+      // })
 
     console.log(target ? (target as HTMLSelectElement).value : '' )
   }
