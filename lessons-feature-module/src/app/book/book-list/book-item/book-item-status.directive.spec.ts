fc4ed0ecaef7c40c3653711f0b7492b69e23d343
lfdantoni <lfdantoni@gmail.com>
2023-11-28 20:13:49 -0300

    clean master branch

--- lessons-feature-module/src/app/book/book-list/book-item/book-item-status.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BookItemStatusDirective } from './book-item-status.directive';
-
-describe('BookItemStatusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BookItemStatusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- lessons-feature-module/src/app/core/models/paypal-config.ts
+++
@@ -1,5 +0,0 @@
-export interface PayPalConfig {
-  url: string;
-  clientId: string;
-  secretId: string;
-}
